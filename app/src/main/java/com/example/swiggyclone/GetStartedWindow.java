fb07845352aabfaed05d863560ef08e3ae56b4b9
SAMIKSHA <samikshark2000@gmail.com>
2024-02-02 00:19:23 +0530

    Update UI: added bg video and improved layout

--- app/src/main/java/com/example/swiggyclone/GetStartedWindow.java
+++ app/src/main/java/com/example/swiggyclone/GetStartedWindow.java
@@ -1,14 +1,61 @@
 package com.example.swiggyclone;
 
+import androidx.annotation.ColorRes;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.VideoView;
 
-public class GetStartedWindow extends AppCompatActivity {
+public class GetStartedWindow extends AppCompatActivity
+{
+    VideoView bgvideo;
+    TextView txt_food,txt_instamart,txt_dineout;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_get_started_window);
+        bgvideo = findViewById(R.id.bgvideo);
+        txt_food = findViewById(R.id.txt_food);
+        txt_instamart = findViewById(R.id.txt_instamart);
+        txt_dineout = findViewById(R.id.txt_dineout);
+
+        //Background Video
+        String path = "android.resource://com.example.swiggyclone/"+R.raw.foodvideo;
+        Uri uri= Uri.parse(path);
+        bgvideo.setVideoURI(uri);
+        bgvideo.start();
+        bgvideo.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mediaPlayer) {
+                mediaPlayer.setLooping(true);
+            }
+        });
+
+    }
+
+    @Override
+    protected void onResume()
+    {
+        bgvideo.resume();
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause()
+    {
+        bgvideo.suspend();
+        super.onPause();
+    }
+
+    @Override
+    protected void onDestroy()
+    {
+        bgvideo.stopPlayback();
+        super.onDestroy();
     }
 }
