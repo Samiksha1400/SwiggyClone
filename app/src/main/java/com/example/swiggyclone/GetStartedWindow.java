0c83f93e728549d2b9fd4dadcb292171663ab81e
SAMIKSHA <samikshark2000@gmail.com>
2024-02-01 18:34:45 +0530

    Added Splash Screen, Get Started Activity , Theme

---
+++ app/src/main/java/com/example/swiggyclone/GetStartedWindow.java
@@ -0,0 +1,14 @@
+package com.example.swiggyclone;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GetStartedWindow extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_get_started_window);
+    }
+}
