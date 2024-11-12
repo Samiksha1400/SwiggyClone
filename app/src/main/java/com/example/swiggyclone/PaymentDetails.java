148e4221a722e09e4907fc043e858a8f62f7129a
Samiksha Kudachikar <samikshark2000@gmail.com>
2024-11-12 20:24:08 +0530

    Added Razorpay Payment Gateway

--- app/src/main/java/com/example/swiggyclone/PaymentDetails.java
+++ app/src/main/java/com/example/swiggyclone/PaymentDetails.java
@@ -1,6 +1,7 @@
 package com.example.swiggyclone;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +9,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class PaymentDetails extends AppCompatActivity {
+import com.razorpay.Checkout;
+import com.razorpay.PaymentResultListener;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class PaymentDetails extends AppCompatActivity implements PaymentResultListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +27,37 @@ public class PaymentDetails extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        Checkout.preload(getApplicationContext());
+
+        startPayment();
+    }
+
+    private void startPayment(){
+        Checkout checkout = new Checkout();
+        checkout.setKeyID("rzp_test_UKjnqIlIx2eD48");
+
+        JSONObject jsonObject = new JSONObject();
+        try {
+            jsonObject.put("name","Swiggy Clone");
+            jsonObject.put("description","Order PAyment");
+            jsonObject.put("currency","INR");
+            jsonObject.put("amount","10000");
+
+            checkout.open(this,jsonObject);
+        } catch (JSONException e)
+        {
+            Toast.makeText(this, "Error"+e.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public void onPaymentSuccess(String s) {
+        Toast.makeText(this, "Payment Successful"+s, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onPaymentError(int i, String s) {
+        Toast.makeText(this, "Payment Failed"+s, Toast.LENGTH_SHORT).show();
     }
 }
